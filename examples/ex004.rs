7ddd70c0d20fc901b8d270349a284a3034e81d06
UESU, Daisuke <uesud@live.jp>
2020-01-08 22:59:04 +0900

    Ex.4: 与えられた正の整数より小さい最大の素数

---
+++ examples/ex004.rs
@@ -0,0 +1,15 @@
+//! 与えられた整数より小さな最大の素数を返すサンプルコード
+
+use rust_callenge::math::largest_prime;
+
+mod common;
+use common::read_u32;
+
+pub fn main() {
+    let n = read_u32("Upper limit");
+    
+    match largest_prime(n) {
+        Some(x) => println!("result = {}", x),
+        None => println!("None"),
+    }    
+}
--- src/math.rs
+++ src/math.rs
@@ -49,3 +49,65 @@ pub fn lcm(a: u32, b: u32) -> u32 {
 fn test_ldm() {
     assert_eq!(15, lcm(3, 5));
 }
+
+/// 与えられた `n` より小さな素数のリストを返す
+fn prime(n: u32) -> Vec<u32> {
+
+    // エラトステネスの篩で素数の一覧を求める
+
+    let limit = (n as f64).sqrt().floor() as u32;
+    let mut v = (2..=n).rev().collect::<Vec<u32>>();
+    let mut p = Vec::with_capacity(limit as usize);
+
+    loop {
+        match v.pop() {
+            Some(x) => {
+                // xを素数リストに加える
+                p.push(x);
+
+                // xの倍数をvから除外する
+                for i in (0..v.len()).rev() {
+                    if v[i] % x == 0 {
+                        v.remove(i);
+                    }
+                }
+
+                // 閾値まできたら探索を打ち切り、vの残りを素数リストに加える
+                if x > limit {
+                    v.reverse();
+                    p.extend(v);
+                    break;    
+                }
+            },
+            None => break,
+        }
+    }
+
+    // n が素数の場合はpに含まれるので除外する
+    if Some(&n) == p.last() {
+        p.pop();
+    }
+
+    p
+}
+
+#[test]
+fn test_prime() {
+    assert_eq!(prime(1), []);
+    assert_eq!(prime(2), []);
+    assert_eq!(prime(3), [2]);
+    assert_eq!(prime(11), [2,3,5,7]);
+}
+
+/// 与えられた `n` より小さな最大の素数を返す
+pub fn largest_prime(n: u32) -> Option<u32> {
+    prime(n).pop()
+}
+
+#[test]
+fn test_largest_prime() {
+    assert_eq!(largest_prime(1), None);
+    assert_eq!(largest_prime(2), None);
+    assert_eq!(largest_prime(3), Some(2));
+    assert_eq!(largest_prime(3572), Some(3571));
+}
